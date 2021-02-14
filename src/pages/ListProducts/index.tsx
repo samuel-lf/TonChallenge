76305898733ee8f283a37996733a03e75b13f591
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-02-14 19:59:59 -0300

    Criação da tela de listagem de produtos

--- src/pages/ListProducts/index.tsx
+++ src/pages/ListProducts/index.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import CardProduct from '../../components/CardProduct';
+import Button from '../../components/Button';
+import { Container, ContainerProducts } from './styles';
+import getFoods from '../../services/foods';
+import IProduct from '../../models/IProduct';
 
-const ListProducts: React.FC = () => (
-  <>
-    <View>
-      <Text>Aqui</Text>
-    </View>
-  </>
-);
+const ListProducts: React.FC = () => {
+  const [foods, setFoods] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    async function anyNameFunction(): Promise<void> {
+      const response = await getFoods();
+      setFoods(
+        response.data.map((food: IProduct) => ({
+          ...food,
+        })),
+      );
+    }
+    anyNameFunction();
+  }, []);
+
+  return (
+    <>
+      <Container horizontal>
+        <ContainerProducts<React.ElementType>
+          data={foods}
+          keyExtractor={(item: IProduct) => String(item.id)}
+          // eslint-disable-next-line react/no-unused-prop-types
+          renderItem={({ item }: { item: IProduct }) => (
+            <CardProduct
+              imageURI={item.imageUrl}
+              productName={item.name}
+              description={item.description}
+              price={item.price}
+            >
+              <Button>Adicionar ao carrinho</Button>
+            </CardProduct>
+          )}
+        />
+      </Container>
+    </>
+  );
+};
 
 export default ListProducts;
--- src/pages/ListProducts/styles.ts
+++ src/pages/ListProducts/styles.ts
@@ -1,3 +1,10 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export const Container = styled.view``;
+export const Container = styled(ScrollView)`
+  margin-bottom: 10px
+`;
+
+export const ContainerProducts = styled.FlatList`
+  padding: 10px;
+`;
