2c007542290a85dd05876549af6c794b6147ced0
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-02-14 02:52:40 -0300

    Criação da função de formatar valor para real-BRL

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { View } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
+
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
-    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+    <View style={{ flex: 1 }}>
       <Routes />
     </View>
   </NavigationContainer>
---
+++ src/utils/formatValueToBRL.ts
@@ -0,0 +1,6 @@
+const formatValueToBRL = (value: number): string => Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+}).format(value);
+
+export default formatValueToBRL;
