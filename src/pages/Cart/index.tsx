d6ec2e427ce1b822fba4349a6bf0d8d0870051a5
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-02-15 10:02:45 -0300

    Criação da tela do carrinho e funcionalidade remoção de itens

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,10 +1,69 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import CardProduct from '../../components/CardProduct';
+import Button from '../../components/Button';
+import {
+  Container, ContainerNoHasItens,
+  ContainerProducts, ContainerTotal, NumberTotal, TextNoHasItens, TextTotal,
+} from './styles';
+import IProduct from '../../models/IProduct';
+import formatValueToBRL from '../../utils/formatValueToBRL';
 
-const Cart: React.FC = () => (
-  <View>
-    <Text>Aqui</Text>
-  </View>
-);
+Icon.loadFont();
+
+const Cart: React.FC = () => {
+  const foods = useSelector((state:IProduct[]) => state);
+  const dispach = useDispatch();
+
+  const removeProduct = (food: IProduct) => {
+    dispach({ type: 'REMOVE_FROM_CART', payload: food });
+  };
+
+  const verifyTotal = () => {
+    const total = foods.reduce((acc, item) => acc + item.price, 0);
+
+    return formatValueToBRL(total);
+  };
+
+  return (
+    <>
+      {
+      foods.length !== 0 ? (
+        <>
+          <ContainerTotal>
+            <TextTotal>Total</TextTotal>
+            <NumberTotal>{verifyTotal()}</NumberTotal>
+          </ContainerTotal>
+          <Container horizontal>
+            <ContainerProducts<React.ElementType>
+              data={foods}
+              keyExtractor={(food: IProduct) => String(food.id)}
+              // eslint-disable-next-line react/no-unused-prop-types
+              renderItem={({ item }: { item: IProduct }) => (
+                <CardProduct
+                  imageURI={item.imageUrl}
+                  productName={item.name}
+                  description={item.description}
+                  price={item.price}
+                >
+                  <Button onPress={() => { removeProduct(item); }} background="danger">
+                    Remover ao carrinho
+                  </Button>
+                </CardProduct>
+              )}
+            />
+          </Container>
+        </>
+      ) : (
+        <ContainerNoHasItens>
+          <TextNoHasItens>Carrinho vazio</TextNoHasItens>
+          <Icon name="emoticon-sad-outline" size={50} color="#bbbb" />
+        </ContainerNoHasItens>
+      )
+    }
+    </>
+  );
+};
 
 export default Cart;
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,3 +1,41 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
-export const Container = styled.view``;
+export const Container = styled(ScrollView)`
+  margin-bottom: 10px;
+`;
+
+export const ContainerProducts = styled.FlatList`
+  padding: 10px;
+  margin-top: 10px;
+`;
+
+export const ContainerTotal = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 15px;
+  border-bottom-color: #e6e6e6;
+  border-bottom-width: 1px;
+`;
+
+export const TextTotal = styled.Text`
+  font-size: 18px;
+  color: #52b788;
+`;
+
+export const NumberTotal = styled.Text`
+  font-size: 24px;
+  color: #52b788;
+  font-weight: bold;
+`;
+
+export const ContainerNoHasItens = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextNoHasItens = styled.Text`
+  color: #bbbb;
+  font-size: 30px;
+`;
