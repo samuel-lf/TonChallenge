817b177e02418187f29ed2d017ead0ef2ae5a98a
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-02-16 11:55:11 -0300

    Refatoração da página ListProducts

--- src/pages/ListProducts/index.tsx
+++ src/pages/ListProducts/index.tsx
@@ -1,51 +1,61 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CardProduct from '../../components/CardProduct';
 import Button from '../../components/Button';
-import { Container, ContainerProducts } from './styles';
 import getFoods from '../../services/foods';
-import IProduct from '../../models/IProduct';
+import { addProductsToList, listAddProductToCart } from '../../stores/actions/listProducts';
+import IState from '../../models/IState';
+import Product from '../../models/IProductInList';
+
+import { Container, ContainerProducts, LoadingText } from './styles';
+import { cartAddProduct } from '../../stores/actions/cartProducts';
 
 const ListProducts: React.FC = () => {
-  const [foods, setFoods] = useState<IProduct[]>([]);
-  const dispach = useDispatch();
+  const [loadingScreen, setLoadingScreen] = useState(true);
+  const listProducts = useSelector((state:IState) => state.listProducts);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    async function requestGetFoods(): Promise<void> {
-      const response = await getFoods();
-      setFoods(
-        response.data.map((food: IProduct) => ({
-          ...food,
-        })),
-      );
+    function requestGetFoods(): void {
+      getFoods().then((response) => {
+        dispatch(addProductsToList(response.data));
+        setLoadingScreen(false);
+      });
     }
     requestGetFoods();
-  }, []);
+  }, [dispatch]);
 
-  const addProduct = (item: IProduct) => {
-    dispach({ type: 'ADD_TO_CART', payload: item });
+  const addProductToCart = (item: Product) => {
+    dispatch(listAddProductToCart([item]));
+    dispatch(cartAddProduct([item]));
   };
 
   return (
     <>
-      <Container horizontal>
-        <ContainerProducts<React.ElementType>
-          data={foods}
-          keyExtractor={(item: IProduct) => String(item.id)}
-          // eslint-disable-next-line react/no-unused-prop-types
-          renderItem={({ item }: { item: IProduct }) => (
-            <CardProduct
-              imageURI={item.imageUrl}
-              productName={item.name}
-              description={item.description}
-              price={item.price}
-            >
-              <Button onPress={() => { addProduct(item); }} background="primary">
-                Adicionar ao carrinho
-              </Button>
-            </CardProduct>
-          )}
-        />
+      <Container>
+        {
+          loadingScreen === false ? (
+            <ContainerProducts<React.ElementType>
+              data={listProducts}
+              keyExtractor={(item: Product) => String(item.id)}
+              // eslint-disable-next-line react/no-unused-prop-types
+              renderItem={({ item }: { item: Product }) => (
+                <CardProduct
+                  imageURI={item.imageUrl}
+                  productName={item.name}
+                  description={item.description}
+                  price={item.price}
+                >
+                  <Button onPress={() => { addProductToCart(item); }} background="primary" enabled={!item.addedToCart}>
+                    {!item.addedToCart ? 'Adicionar ao carrinho' : 'Adicionado ao carrinho'}
+                  </Button>
+                </CardProduct>
+              )}
+            />
+          ) : (
+            <LoadingText>Carregando...</LoadingText>
+          )
+        }
       </Container>
     </>
   );
--- src/pages/ListProducts/styles.ts
+++ src/pages/ListProducts/styles.ts
@@ -1,10 +1,17 @@
 import styled from 'styled-components/native';
-import { ScrollView } from 'react-native-gesture-handler';
 
-export const Container = styled(ScrollView)`
-  margin-bottom: 10px
+export const Container = styled.View`
+  margin-bottom: 10px;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const ContainerProducts = styled.FlatList`
   padding: 10px;
+`;
+
+export const LoadingText = styled.Text`
+  color: #bbbb;
+  font-size: 30px;
 `;
